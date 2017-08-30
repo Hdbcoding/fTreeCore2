c549468dcefd8a707a0ea7a5eca96606e9812da7
henry <hdbyrd1@gmail.com>
2017-08-30 10:54:02 +0800

    Added basic mvc Startup Added a functioning controller Started putting together views

--- fTreeCore2.Web/Startup.cs
+++ fTreeCore2.Web/Startup.cs
@@ -15,6 +15,7 @@ namespace fTreeCore2.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -23,11 +24,21 @@ namespace fTreeCore2.Web
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                //todo - write error pages?
+                app.UseExceptionHandler("/Home/Error");
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
+            app.UseStaticFiles();
+
+            app.UseMvc(routes => {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}"
+                );
             });
         }
     }
