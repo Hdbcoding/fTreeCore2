44a082aac2762f25fcba8f33dbff2ac18fcbc86d
henry <henry.byrd@claricode.com>
2018-07-19 22:10:47 -0400

    nevermind, gulp-bundle-assets won't be good because it doesn't like es6 Let's just do the bundling more by hand

--- fTreeCore2.Web/gulpfile.js
+++ fTreeCore2.Web/gulpfile.js
@@ -1,5 +1,4 @@
-var gulp = require('gulp'),
-    bundle = require('gulp-bundle-assets');
+var gulp = require('gulp');
 
 gulp.task('copyCss', function () {
     return gulp.src(['./node_modules/bootstrap/dist/css/*.css'])
@@ -8,14 +7,50 @@ gulp.task('copyCss', function () {
 
 gulp.task('copyJs', function () {
     return gulp.src([
-        './node_modules/jquery/dist/*.js',
-        './node_modules/popper.js/dist/*.js',
-        './node_modules/bootstrap/dist/js/*.js'
+        './node_modules/jquery/dist/jquery*js',
+        './node_modules/popper.js/dist/popper*js',
+        './node_modules/bootstrap/dist/js/bootstrap*js'
     ]).pipe(gulp.dest('./wwwroot/lib/js'));
 });
 
-gulp.task('bundle', function () {
-    return gulp.src('bundle.config.js')
-        .pipe(bundle())
-        .pipe(gulp.dest('./wwwroot'));
-});
+//using gulp-bundle-assets
+// gulp.task('bundle', function () {
+//     return gulp.src('bundle.config.js')
+//         .pipe(bundle())
+//         .pipe(gulp.dest('./wwwroot'));
+// });
+
+// {
+//     "label": "gulpBundles",
+//     "type": "shell",
+//     "command": "gulp bundle",
+//     "options": {
+//         "cwd": "${workspaceRoot}/fTreeCore2.Web"
+//     },
+//     "dependsOn": [
+//         "yarn"
+//     ]
+// },
+
+//from old bundle.config.js
+// bundle: {
+//     familyTree: {
+//         scripts: [
+//             './wwwroot/js/canvas.js',
+//             './wwwroot/js/visualElement.js',
+//             './wwwroot/js/event.js'
+//         ],
+//         styles: ['./wwwroot/css/site.css']
+//     },
+//     lib: {
+//         scripts: [
+//             './wwwroot/lib/js/jquery.slim.min.js',
+//             './wwwroot/lib/js/popper.min.js',
+//             './wwwroot/lib/js/bootstrap.min.js'
+//         ],
+//         styles: ['./wwwroot/lib/css/bootstrap.min.css'],
+//         options: {
+//           maps: false
+//         }
+//     }
+// }
